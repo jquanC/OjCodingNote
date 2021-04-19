033fa5bd69a8c8620a989a7d3b11371252aa4dc3
quan <jack_cai.sh@foxmail.com>
2021-04-19 22:20:30 +0800

    0419-OJ score:9

---
+++ src/Hot100/Easy/TestSol6.java
@@ -0,0 +1,10 @@
+package Hot100.Easy;
+
+public class TestSol6 {
+    public static void main(String args[]){
+        int [] arrTest = {0,1,0,3,12};
+        Solution6 sol6 = new Solution6();
+        sol6.moveZeroes(arrTest);
+
+    }
+}
