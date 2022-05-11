fec872e1d2e96bf12ee162794e63a3a413bf247c
jquanC <jack_cai.sh@foxmail.com>
2022-05-11 21:55:37 +0800

    after reinstall os

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,28 +1,36 @@
 package test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
 //        System.out.println(testM());
-      
-            Scanner sc = new Scanner(System.in);
-            int n = sc.nextInt();
-            int m =sc.nextInt();
-            sc.nextLine();
-            int q = sc.nextInt();
-            System.out.println(n);
-            System.out.println(m);
-            System.out.println(q);
 
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        arrayList.add(1);
+        arrayList.add(2);
+        List<? extends Number> intList = arrayList;//直接 List<? extends Number> intList  = new ArrayList<Integer>(); 无法存入
+        Number firstNum = intList.get(0);
+        System.out.println(firstNum);
 
+        List<? super Number>  superList = new ArrayList<>();
+        superList.add(2.9);
+        superList.add(3);
+        Number firNum = (Number) superList.get(0);
+        Object secNum = superList.get(1);
+        System.out.println(firNum);
+        System.out.println(secNum);
+        System.out.println("after reinstall");
     }
-    public static int testM(){
-        int i=0;
-        try{
-            i=6;
-        }finally {
+
+    public static int testM() {
+        int i = 0;
+        try {
+            i = 6;
+        } finally {
             i++;
             return i;
         }
